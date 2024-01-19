a93feaba508dd307825a7a56627d11d1a29b668a
Hillary <hrams31@wgu.edu>
2024-01-19 14:07:31 -0500

    updates to model.py

--- ml/model.py
+++ ml/model.py
@@ -1,6 +1,9 @@
 import pickle
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
-from ml.data import process_data
+from ml.data import process_data  # Assuming ml is a package and process_data is a module within that package
+import pandas as pd
+
 # TODO: add necessary import
 
 # Optional: implement hyperparameter tuning.
@@ -19,8 +22,13 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
-   # TODO: implement the function
-    pass
+    # TODO: implement the function
+    model = RandomForestClassifier(random_state=1089)
+    model.fit(X_train, y_train)
+
+    return model
+
+    
 
 
 def compute_model_metrics(y, preds):
@@ -50,7 +58,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : RandomForestClassifier
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -59,8 +67,7 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    # TODO: implement the function
-    pass
+    return model.predict(X)
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -73,12 +80,16 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
-    pass
+    with open(path, 'wb') as file:
+        pickle.dump(model, file)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
+    """Pickle file is loaded from path and returns it"""
+    with open(path, 'rb') as file:
+        loaded_model = pickle.load(file)
+    return loaded_model
 
 
 def performance_on_categorical_slice(
@@ -107,7 +118,7 @@ def performance_on_categorical_slice(
         Trained sklearn OneHotEncoder, only used if training=False.
     lb : sklearn.preprocessing._label.LabelBinarizer
         Trained sklearn LabelBinarizer, only used if training=False.
-    model : ???
+    model : RandomForestClassifier
         Model used for the task.
 
     Returns
@@ -118,11 +129,14 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
-    X_slice, y_slice, _, _ = process_data(
-        # your code here
-        # for input data, use data in column given as "column_name", with the slice_value 
-        # use training = False
-    )
-    preds = # your code here to get prediction on X_slice using the inference function
+    
+    X_slice, y_slice, _, _ = process_data(data, categorical_features, label, 
+                                          encoder, lb, training=False, 
+                                          column_name=column_name, 
+                                          slice_value=slice_value)
+    
+    # Predictions
+    
+    preds = inference(model, X_slice)
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
